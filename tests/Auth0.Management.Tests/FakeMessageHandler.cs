6b708c7a962397220b63a2ca999ea767536f7ca3
Patrick Evers <pmdevers@hotmail.com>
2020-09-15 15:19:35 +0200

    Fake oauth/token request

--- tests/Auth0.Management.Tests/FakeMessageHandler.cs
+++ tests/Auth0.Management.Tests/FakeMessageHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
@@ -18,6 +19,24 @@ namespace Auth0.Management.Tests
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
+            if (request.RequestUri.AbsolutePath.Equals("/oauth/token"))
+            {
+                return new HttpResponseMessage()
+                  
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(
+                        "{ " +
+                        "   \"access_token\":\"eyJz93a...k4laUWw\", " +
+                        "   \"refresh_token\":\"GEbRxBN...edjnXbL\"," +
+                        "   \"id_token\":\"eyJ0XAi...4faeEoQ\"," +
+                        "   \"token_type\":\"Bearer\"," +
+                        "   \"expires_in\":86400" +
+                        "}", Encoding.UTF8,
+                        "application/json")
+                };
+            }
+
             return await Task.FromResult(_fakeResponse);
         }
     }
